b7827e513ca895617316521888be45f62be7803e
Jatin-J-Patel <117100368+Jatin-J-Patel@users.noreply.github.com>
2023-01-24 17:50:14 +0000

    Changes pt2

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <hgroup>
           <img src="images/group.svg" alt ="StarGazers Group" />
           <Welcome name="Stars" />
-          <Hello name="StarGazers" />
+          <Welcome name="StarGazers" />
           <Welcome name="StarAliens" />
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={()=> alert('Hi There')}>Click me</button>
